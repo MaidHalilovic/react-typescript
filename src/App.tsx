787213600a51c2727e60db8075f2459dc89af80d
MaidHalilovic <maidhaliloviclimunada567@gmail.com>
2025-09-03 00:07:17 +0200

    Learned about UseState

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import "./App.css";
+import type { User } from "./interfaces";
 
 function App() {
+  const [user, setUser] = useState<User | null>(null);
+
+  const fetchUser = () =>
+    setUser({
+      name: "Maidh",
+      age: 20,
+      country: "Serbia",
+      adress: {
+        street: "ras 7",
+        number: 7,
+        city: "Novi Pazar",
+      },
+      admin: false,
+    });
+
   return (
     <>
-      <h1>UseState</h1>
+      <button onClick={fetchUser}>Fetch user on onClick</button>
+      {user && <p>{`Welcome ${user.name}`}</p>}
     </>
   );
 }
---
+++ src/interfaces.ts
@@ -0,0 +1,13 @@
+export interface Address {
+  street: string;
+  number: number;
+  city: string;
+}
+
+export interface User {
+  name: string;
+  age: number;
+  country: string;
+  adress: Address;
+  admin: boolean;
+}
