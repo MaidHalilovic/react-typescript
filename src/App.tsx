e8fcb1ef2e2c131b5ccb242e81f3b7d09dbb0f19
MaidHalilovic <maidhaliloviclimunada567@gmail.com>
2025-09-07 21:44:55 +0200

    Learning difference between interface and type

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
-import type { User } from "./interfaces";
+// import type { User } from "./interfaces";
 
 function App() {
-  const [user, setUser] = useState<User | null>(null);
+  // const [user, setUser] = useState<User | null>(null  );
 
-  const fetchUser = () =>
-    setUser({
-      name: "Maidh",
-      age: 20,
-      country: "Serbia",
-      adress: {
-        street: "ras 7",
-        number: 7,
-        city: "Novi Pazar",
-      },
-      admin: false,
-    });
+  // const fetchUser = () =>
+  //   setUser({
+  //     name: "Maidh",
+  //     age: 20,
+  //     country: "Serbia",
+  //     adress: {
+  //       street: "ras 7",
+  //       number: 7,
+  //       city: "Novi Pazar",
+  //     },
+  //     admin: false,
+  //   });
 
   return (
     <>
-      <button onClick={fetchUser}>Fetch user on onClick</button>
-      {user && <p>{`Welcome ${user.name}`}</p>}
+      {/* <button onClick={fetchUser}>Fetch user on onClick</button>
+      {user && <p>{`Welcome ${user.name}`}</p>} */}
     </>
   );
 }
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,13 +1,13 @@
-export interface Address {
-  street: string;
-  number: number;
-  city: string;
-}
+// export interface Address {
+//   street: string;
+//   number: number;
+//   city: string;
+// }
 
-export interface User {
-  name: string;
-  age: number;
-  country: string;
-  adress: Address;
-  admin: boolean;
-}
+// export interface User {
+//   name: string;
+//   age: number;
+//   country: string;
+//   adress: Address;
+//   admin: boolean;
+// }
